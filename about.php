6ae3c3668f1b4e6f98625a7ec02c75942f10a171
Akash Singh <aakash.s.aes@gmail.com>
2024-08-21 13:05:08 +0530

    about page modified

--- about.php
+++ about.php
@@ -33,7 +33,8 @@
                         <img src="assets/images/about-inner/about-thumb-2.png" alt="">
                     </div>
                     <div class="about-video-box">
-                        <a class="video-vemo-icon venobox vbox-item" data-vbtype="youtube" data-autoplay="true" href="https://www.youtube.com/watch?v=e6R6VsgD8yQ&amp;amp;t=179s"><i class="bi bi-play-fill"></i></a>
+                        <img src="./assets/images/about-video-r.jpg" alt="" class="about-video">
+                        <!-- <a class="video-vemo-icon venobox vbox-item" data-vbtype="youtube" data-autoplay="true" href="https://www.youtube.com/watch?v=e6R6VsgD8yQ&amp;amp;t=179s"><i class="bi bi-play-fill"></i></a> -->
                     </div>
                 </div>
             </div>
@@ -109,51 +110,8 @@
                     </div>
                 </div> -->
             </div>
-            <div class="col-lg-3 col-md-6 col-sm-6">
-                <div class="single-pricing-box">
-                    <div class="pricing-icon">
-                        <img src="assets/images/home-3/pricing-icon.png" alt="">
-                    </div>
-                    <div class="pricing-content">
-                        <h4>Busines Plan</h4>
-                        <p>The markets and front market</p>
-                        <h1>$30 <span>/ Month</span> </h1>
-                    </div>
-                    <div class="pricing-list">
-                        <ul>
-                            <li>10+ user Account</li>
-                            <li>Moneyback Gaurentee</li>
-                            <li>Unlimited Database</li>
-                            <li>24/7 Supports</li>
-                        </ul>
-                    </div>
-                    <div class="pricing-button">
-                        <a href="#">Purchaces</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6 col-sm-6">
-                <div class="single-pricing-box two">
-                    <div class="pricing-icon">
-                        <img src="assets/images/home-3/pricing-icon.png" alt="">
-                    </div>
-                    <div class="pricing-content">
-                        <h4>Popular Plan</h4>
-                        <p>The markets and front market</p>
-                        <h1>$10 <span>/ Month</span> </h1>
-                    </div>
-                    <div class="pricing-list">
-                        <ul>
-                            <li>10+ user Account</li>
-                            <li>Moneyback Gaurentee</li>
-                            <li>Unlimited Database</li>
-                            <li>24/7 Supports</li>
-                        </ul>
-                    </div>
-                    <div class="pricing-button">
-                        <a href="#">Purchaces</a>
-                    </div>
-                </div>
+            <div class="col-lg-6 col-md-6 col-sm-6">
+                <img src="./assets/images/about-right.jpg" alt="" class="about-right">
             </div>
         </div>
     </div>
@@ -244,8 +202,8 @@
         </div>
         <div class="row">
             <div class="col-lg-6">
-                <div class="mission-thumb">
-                    <img src="assets/images/inner-images/mission-1.png" alt="">
+                <div class="mission-thumb" style="height: 100%;">
+                    <img src="./assets/images/about-goal.jpg" alt="" style="height: 100%; object-fit:cover">
                 </div>
             </div>
             <div class="col-lg-6">
@@ -294,13 +252,12 @@
                     <img src="assets/images/home-3/contact-icon.png" alt="">
                 </div>
                 <div class="contact-content">
-                    <h6>Call Us Anytime</h6>
-                    <h1>+980 123 (4567) 890</h1>
-                    <p>Professionally optimize interdependent intellectual interoperable
-                        best practices. Progressively fabricate</p>
+                    <h6>Need Help? Just Call. Anytime.</h6>
+                    <h1>+91-9999999999</h1>
+                    <p>Got an idea? Our team of skilled developers can turn it into a reality. Share your vision, and we'll bring it to life. Contact us today.</p>
                 </div>
                 <div class="toptech-button inner-style">
-                    <a href="#">Let’s Talk<i class="bi bi-arrow-right-short"></i></a>
+                    <a href="contact">Let’s Talk<i class="bi bi-arrow-right-short"></i></a>
                 </div>
             </div>
             <div class="col-lg-6">
@@ -308,9 +265,9 @@
                     <div class="contact-thumb">
                         <img src="assets/images/home-3/contact-thumb.png" alt="">
                     </div>
-                    <div class="contact-shape">
+                    <!-- <div class="contact-shape">
                         <img src="assets/images/home-3/contact-shape.png" alt="">
-                    </div>
+                    </div> -->
                 </div>
             </div>
         </div>
@@ -318,50 +275,7 @@
 </div>
 <!-- End Toptech Contact info Area Style Three-->
 
-<!-- Start Toptech Counter Area Home Three-->
-<!-- <div class="counter-area style-three">
-    <div class="container">
-        <div class="row add-bg">
-            <div class="col-lg-3 col-md-6 col-sm-6">
-                <div class="single-counter-box">
-                    <div class="counter-content">
-                        <h4>960</h4>
-                        <span>+</span>
-                        <p>Active Coustomer </p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6 col-sm-6">
-                <div class="single-counter-box">
-                    <div class="counter-content">
-                        <h4>90</h4>
-                        <span>+</span>
-                        <p>Expert Members </p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6 col-sm-6">
-                <div class="single-counter-box">
-                    <div class="counter-content">
-                        <h4>852</h4>
-                        <span>+</span>
-                        <p>Satisfied Customers </p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6 col-sm-6">
-                <div class="single-counter-box last-child">
-                    <div class="counter-content">
-                        <h4>100</h4>
-                        <span>%</span>
-                        <p>Satisfaction Rate </p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div> -->
-<!-- End Toptech Counter Area Style Three-->
+
 
 
 
