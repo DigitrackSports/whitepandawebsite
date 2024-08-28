c8b44fba6d76af8e3e72a53bc6e4c4de14571da0
Akash Singh <aakash.s.aes@gmail.com>
2024-08-28 15:16:50 +0530

    mail issue

--- contact2.php
+++ contact2.php
@@ -108,10 +108,9 @@ if (isset($_POST['email'])) {
     $mail->setFrom('aakash.s.aes@gmail.com');
     $mail->addAddress('akashraj608@gmail.com');
 
-
+    $mail->isHTML(true);
     $mail->Subject = 'Enquiry from WhitePanda Website';
     $mail->Body = $email_message;
-    $mail->isHTML(true);
 
     if (!$mail->send()) {
         // echo $result="not send";
